da1aef62560d5c4797780167b1d7df5c0a77a8c2
masawdah <masawdah@gmail.com>
2023-07-24 16:11:50 +0200

    fix & test the esmf setup

--- xagg/esmf_setup.py
+++ xagg/esmf_setup.py
@@ -11,10 +11,11 @@ def get_env_info():
 def set_esmf_mk_path():
     env_name, env_path = get_env_info()
     if env_path:
-        esmf_mk_path = os.path.join(env_path, 'lib', 'esmf.mk')
+        if env_path.endswith('/bin'): env_path= env_path[:-4]
+        esmf_mk_path = os.path.join(env_path,'lib', 'esmf.mk')
         if os.path.isfile(esmf_mk_path):
             os.environ['ESMFMKFILE'] = esmf_mk_path
-           # print(f"Found esmf.mk at: {esmf_mk_path}")
+            print(f"Found esmf.mk at: {esmf_mk_path}")
         else:
             print('esmf.mk not found in the specified conda environment.')
     else:
@@ -22,15 +23,5 @@ def set_esmf_mk_path():
         
 set_esmf_mk_path()
     
-
-#if __name__ == "__main__":
-#    env_name, env_path = get_env_info()
-#    if env_path:
- #       esmf_mk_path = os.path.join(env_path, 'lib', 'esmf.mk')
-#    if esmf_mk_path:
- #       os.environ['ESMFMKFILE'] = esmf_mk_path
- #       import esmpy
- #   else:
-  #      print('esmf.mk not found in the specifieds conda environmnet.')
         
         
