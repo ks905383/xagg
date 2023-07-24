96455fe2c34426d50fa38318b8d51e66605e91e0
masawdah <masawdah@gmail.com>
2023-07-24 12:31:06 +0200

    esmf setup

--- xagg/__init__.py
+++ xagg/__init__.py
@@ -2,6 +2,7 @@
 # everything else happening behind the scenes (and the exporting 
 # happening as methods to the classes that are exported from those
 # two functions)
+import xagg.esmf_setup
 from .wrappers import pixel_overlaps
 from .aux import (normalize,fix_ds,get_bnds,subset_find)
 from .core import (aggregate,read_wm)
